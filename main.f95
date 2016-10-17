9d9a77ef46023db748030998bf46d61013b958c8
pbtflakes <pbtflakes@protonmail.ch>
2016-10-17 19:05:18 -0400

    heck

--- main.f95
+++ main.f95
@@ -11,19 +11,19 @@ program fibonacci
                 
                 recursive function fibo(x) result(fibon)
                         integer, dimension(2) :: fibon
-                        integer :: x, a, b, c, d, e, k
-
+                        integer, dimension(2) :: ab
+                        integer :: x, c, d, e, k
                         if (x == 0) then
                                 fibon(1) = 0
                                 fibon(2) = 1
                         else
                                 e = x / 2
                                 k = 1
-                                a = fibo(e:k)
+                                ab(1) = fibo(e:k)
                                 k = 2
-                                b = fibo(e:k)
-                                c = a * (b * 2 - a)
-                                d = a**2 + b**2
+                                ab(2) = fibo(e:k)
+                                c = ab(1) * (ab(2) * 2 - ab(1))
+                                d = ab(1)**2 + ab(2)**2
                                 if (mod(x,2) == 0) then
                                         fibon(1) = c
                                         fibon(2) = d
